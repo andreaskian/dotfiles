21234f88574c071ecfce86236018ad6f3252b55c
Andreas Kian <andreas@andreaskian.dk>
2024-05-01 23:13:22 +0200

    feat(nvim): update init

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,24 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
--- Mini Move
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
+-- globals
 vim.g.mapleader = " "
+
+-- 2 spaces indentations
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+-- save on cmd + s
+vim.keymap.set({ "i", "x", "n", "s" }, "D-s", ":w<CR>")
+
+-- eslint fix all on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.tsx", "*.ts", "*.jsx", "*.js" },
+  command = "silent! EslintFixAll",
+})
+
+-- move line up/down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move up" })
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -23,4 +34,3 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
-
